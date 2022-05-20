27565f58fa82389f62c4350060baf31230fef8b5
karpushin <karpushin@netcracker.com>
2022-05-20 19:06:10 +0300

    add form submit functionality

--- src/main/java/org/skarpushin/currencyconverter/controller/PresentationController.java
+++ src/main/java/org/skarpushin/currencyconverter/controller/PresentationController.java
@@ -1,14 +1,19 @@
 package org.skarpushin.currencyconverter.controller;
 
 import org.skarpushin.currencyconverter.errorhandler.BadRequestException;
+import org.skarpushin.currencyconverter.model.ConvertIntput;
 import org.skarpushin.currencyconverter.model.SymbolsResponse;
 import org.skarpushin.currencyconverter.thirdparty.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
+
+import static org.skarpushin.currencyconverter.thirdparty.Constants.CONVERT_URI;
 
 @Controller
 public class PresentationController {
@@ -28,7 +33,13 @@ public class PresentationController {
                 throw new BadRequestException("Can't get symbols from external API");
             }
         }
+        model.addAttribute("convertInput", new ConvertIntput());
         model.addAttribute("symbols", currenciesCache);
         return "index";
+    }
+
+    @PostMapping("/")
+    public String convertSubmit(@ModelAttribute ConvertIntput convertInput, Model model) {
+        return "redirect:/" + String.format("convert?from=%s&to=%s&monetaryValue=%s", convertInput.getFrom(), convertInput.getTo(), convertInput.getAmount());
     }
 }
--- src/main/java/org/skarpushin/currencyconverter/thirdparty/Constants.java
+++ src/main/java/org/skarpushin/currencyconverter/thirdparty/Constants.java
@@ -1,7 +1,7 @@
 package org.skarpushin.currencyconverter.thirdparty;
 
 public class Constants {
-    public static final String CONVERT_URI = "https://api.apilayer.com/exchangerates_data/convert?to=%s&from=%s&amount=%s";
+    public static final String CONVERT_URI = "https://api.apilayer.com/exchangerates_data/convert?from=%s&to=%s&amount=%s";
     public static final String SYMBOLS_URI = "https://api.apilayer.com/exchangerates_data/symbols";
 
 }
